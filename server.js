d8ba76f10b458d4bf1bd438ba97e8f85fff2ac3c
kwokhung <chu_kwokhung@yahoo.com.hk>
2013-07-03 15:33:43 +0800

    test

--- server.js
+++ server.js
@@ -1,2 +1,3 @@
 var socketServer = require('./socketServer');
-socketServer.init(8080, 9000);
+socketServer.init(process.env.PORT || 8080, 9000);
+console.log("Server listening on port %d", process.env.PORT || 8080);
